fcbbf4dbb0e964767bf2a762e5a07ad963b2bafd
zhibei <785740487@qq.com>
2025-05-29 03:32:27 +0800

    fix

--- src/main/kotlin/org/gitee/orryx/dao/storage/H2Manager.kt
+++ src/main/kotlin/org/gitee/orryx/dao/storage/H2Manager.kt
@@ -3,4 +3,4 @@ package org.gitee.orryx.dao.storage
 import taboolib.common.io.newFile
 import taboolib.module.database.HostH2
 
-class H2Manager: IStorageManager by MySqlManager(HostH2(newFile(IStorageManager.file, "data")).createDataSource())
+class H2Manager: IStorageManager by MySqlManager(HostH2(newFile(IStorageManager.file, "data", false)).createDataSource())
