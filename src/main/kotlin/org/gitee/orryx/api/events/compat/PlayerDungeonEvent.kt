42a01f6f94b6f68b5df0129d4461855d0117e54b
zhibei <785740487@qq.com>
2025-05-29 02:43:02 +0800

    fix

--- src/main/kotlin/org/gitee/orryx/api/events/compat/PlayerDungeonEvent.kt
+++ src/main/kotlin/org/gitee/orryx/api/events/compat/PlayerDungeonEvent.kt
@@ -2,6 +2,8 @@ package org.gitee.orryx.api.events.compat
 
 import org.bukkit.entity.Player
 import org.serverct.ersha.dungeon.common.api.event.DungeonEvent
+import taboolib.common.platform.Ghost
 import taboolib.platform.type.BukkitProxyEvent
 
+@Ghost
 class PlayerDungeonEvent(val player: Player, val event: DungeonEvent): BukkitProxyEvent()
--- src/main/kotlin/org/gitee/orryx/core/station/triggers/dungeonplus/DungeonEventProxy.kt
+++ src/main/kotlin/org/gitee/orryx/core/station/triggers/dungeonplus/DungeonEventProxy.kt
@@ -3,10 +3,12 @@ package org.gitee.orryx.core.station.triggers.dungeonplus
 import org.gitee.orryx.api.events.compat.PlayerDungeonEvent
 import org.serverct.ersha.dungeon.common.api.event.DungeonEvent
 import org.serverct.ersha.dungeon.common.team.type.PlayerStateType
+import taboolib.common.platform.Ghost
 import taboolib.common.platform.event.SubscribeEvent
 
 object DungeonEventProxy {
 
+    @Ghost
     @SubscribeEvent
     fun proxy(e: DungeonEvent) {
         if (e.isCancelled) return
